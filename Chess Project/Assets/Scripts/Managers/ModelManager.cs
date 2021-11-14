ab3963e73d888315bd3851048046faba7d654f66
chessMeNow <89541066+chessMeNow@users.noreply.github.com>
2021-11-13 19:50:17 -0500

    heur Bug Fix

--- Chess Project/Assets/Scripts/Managers/ModelManager.cs
+++ Chess Project/Assets/Scripts/Managers/ModelManager.cs
@@ -38,7 +38,7 @@ public class ModelManager : MonoBehaviour
     public float duration { get; set; }
     #endregion
 
-    private void Awake() 
+    private void Awake()
     {
         Instance = this;
         selection = new Vector2Int(-1, -1);
@@ -50,15 +50,19 @@ public class ModelManager : MonoBehaviour
 
         //setup static board tile naming and default variables
         chessBoardGridCo = new Dictionary<string, BoardTile>();
+        chessBoardCopyKing = new Dictionary<string, BoardTile>();
+        chessBoardCopyBishop1 = new Dictionary<string, BoardTile>();
+        chessBoardCopyBishop2 = new Dictionary<string, BoardTile>();
+
         heuristics = GetComponent<Heuristics>();
 
         ChessboardTileSetup();
         SpawnAllPieces();
     }
 
-    private void Start() 
+    private void Start()
     {
-        
+
     }
 
     private void Update()
@@ -70,16 +74,16 @@ public class ModelManager : MonoBehaviour
 
     #region INSTANTIATION
     //function to setup the tile position names, tile is occupied status, if occupied by which color/piece type and basic board wide heuirtics for each color.
-    private void ChessboardTileSetup() 
+    private void ChessboardTileSetup()
     {
         BoardTile board = new BoardTile();
         char letterBoard = '0';
         string showB = "";
 
         //setting up tiles and adding to dictionary default values
-        for (int j = 1; j < 9; j++) 
+        for (int j = 1; j < 9; j++)
         {
-            for (int i = 65; i < 73; i++) 
+            for (int i = 65; i < 73; i++)
             {
                 letterBoard = '0';
                 showB = "";
@@ -97,6 +101,36 @@ public class ModelManager : MonoBehaviour
                 chessBoardGridCo[showB].occupiedPieceType = "";
                 chessBoardGridCo[showB].whiteHeuristic = 0;
                 chessBoardGridCo[showB].blackHeuristic = 0;
+
+                chessBoardCopyKing.Add(showB, board);
+                chessBoardCopyKing[showB].isOccupied = false;
+                chessBoardCopyKing[showB].isWhite = false;
+                chessBoardCopyKing[showB].boardPosition.x = i - 65;
+                chessBoardCopyKing[showB].boardPosition.y = j - 1;
+                chessBoardCopyKing[showB].officalBoardPosition = showB;
+                chessBoardCopyKing[showB].occupiedPieceType = "";
+                chessBoardCopyKing[showB].whiteHeuristic = 0;
+                chessBoardCopyKing[showB].blackHeuristic = 0;
+
+                chessBoardCopyBishop1.Add(showB, board);
+                chessBoardCopyBishop1[showB].isOccupied = false;
+                chessBoardCopyBishop1[showB].isWhite = false;
+                chessBoardCopyBishop1[showB].boardPosition.x = i - 65;
+                chessBoardCopyBishop1[showB].boardPosition.y = j - 1;
+                chessBoardCopyBishop1[showB].officalBoardPosition = showB;
+                chessBoardCopyBishop1[showB].occupiedPieceType = "";
+                chessBoardCopyBishop1[showB].whiteHeuristic = 0;
+                chessBoardCopyBishop1[showB].blackHeuristic = 0;
+
+                chessBoardCopyBishop2.Add(showB, board);
+                chessBoardCopyBishop2[showB].isOccupied = false;
+                chessBoardCopyBishop2[showB].isWhite = false;
+                chessBoardCopyBishop2[showB].boardPosition.x = i - 65;
+                chessBoardCopyBishop2[showB].boardPosition.y = j - 1;
+                chessBoardCopyBishop2[showB].officalBoardPosition = showB;
+                chessBoardCopyBishop2[showB].occupiedPieceType = "";
+                chessBoardCopyBishop2[showB].whiteHeuristic = 0;
+                chessBoardCopyBishop2[showB].blackHeuristic = 0;
             }
         }
 
@@ -104,16 +138,19 @@ public class ModelManager : MonoBehaviour
 
     // Function to spawn a piece prefab on a tile and add the object to the list of active objects with correct color tag.
     // Edited By George; added string to function call; added in board tile variables.
-    private void SpawnPiece(int index, Vector2Int position, string piece) {
+    private void SpawnPiece(int index, Vector2Int position, string piece)
+    {
         GameObject pieceObject = Instantiate(piecePrefabs[index], GetTileCenter(position.x, position.y), Quaternion.Euler(-90, 0, 0));
         pieceObject.transform.SetParent(transform);
 
         //adding tags for White and Black pieces. 0-5 index for white, 6-11 index for black. added by george to existing function
-        if (index <= 5) 
+        if (index <= 5)
         {
             pieceObject.tag = "White Pieces";
             chessBoardGridCo[Convert.ToString(Convert.ToChar(position.x + 65) + Convert.ToString(position.y + 1))].isWhite = true;
-        } else {
+        }
+        else
+        {
             pieceObject.tag = "Black Pieces";
             chessBoardGridCo[Convert.ToString(Convert.ToChar(position.x + 65) + Convert.ToString(position.y + 1))].isWhite = false;
         }
@@ -126,7 +163,8 @@ public class ModelManager : MonoBehaviour
 
     // Initilization function to spawn all chess pieces
     //Edited By George; added string of piece type to function calls.
-    private void SpawnAllPieces() {
+    private void SpawnAllPieces()
+    {
         // Spawn White Pieces
         // King
         SpawnPiece(0, new Vector2Int(4, 0), "King");
@@ -175,7 +213,8 @@ public class ModelManager : MonoBehaviour
     }
 
     // Function to link ChessPiece objects with the GameObject models
-    public void LinkModels(ChessPiece[,] board) {
+    public void LinkModels(ChessPiece[,] board)
+    {
         // Link White Pieces
         // King
         pieceLinks.Add(board[4, 0], activePieces[0]);
@@ -226,27 +265,33 @@ public class ModelManager : MonoBehaviour
 
     #region INTERACTION
     // Function to determine what tile the mouse is hovering over, uses z component of raycast as the board is in the x-z plane
-    private void UpdateSelection() {
+    private void UpdateSelection()
+    {
         if (!Camera.main)
             return;
 
         RaycastHit hit;
-        if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 25.0f, LayerMask.GetMask("ChessPlane"))) {
+        if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 25.0f, LayerMask.GetMask("ChessPlane")))
+        {
             selection.x = (int)hit.point.x;
             selection.y = (int)hit.point.z;
-        } else {
+        }
+        else
+        {
             selection.x = -1;
             selection.y = -1;
         }
     }
 
-    public void MoveObject(ChessPiece piece, Vector2Int position, float duration) {
+    public void MoveObject(ChessPiece piece, Vector2Int position, float duration)
+    {
         float step = duration * Time.deltaTime;
         if (piece != null)
             pieceLinks[piece].transform.position = Vector3.Lerp(pieceLinks[piece].transform.position, GetTileCenter(position.x, position.y), step);
     }
 
-    public void RemoveObject(ChessPiece piece) {
+    public void RemoveObject(ChessPiece piece)
+    {
         GameObject go = pieceLinks[piece];
         pieceLinks.Remove(piece);
         activePieces.Remove(go);
@@ -256,8 +301,10 @@ public class ModelManager : MonoBehaviour
 
     #region RENDERING
     // Function to highlight all tiles associated with selected piece
-    public void HighlightAllTiles(Vector2Int position, List<Vector2Int> availableMoves, List<Vector2Int> enemies, Commander commander) {
-        if (commander.IsWhite) {
+    public void HighlightAllTiles(Vector2Int position, List<Vector2Int> availableMoves, List<Vector2Int> enemies, Commander commander)
+    {
+        if (commander.IsWhite)
+        {
             HighlightSelected(position);
             HighlightAvailableMoves(availableMoves);
             HighlightEnemies(enemies);
@@ -266,46 +313,56 @@ public class ModelManager : MonoBehaviour
     }
 
     // Function to highlight the tile of the selected piece
-    private void HighlightSelected(Vector2Int position) {
+    private void HighlightSelected(Vector2Int position)
+    {
         HighlightTile(0, position.x, position.y);
     }
 
     // Function to highlight all available moves with available highlight prefab using a boolean map of the board
-    private void HighlightAvailableMoves(List<Vector2Int> moves) {
-        foreach (Vector2Int pos in moves) {
+    private void HighlightAvailableMoves(List<Vector2Int> moves)
+    {
+        foreach (Vector2Int pos in moves)
+        {
             HighlightTile(1, pos.x, pos.y);
         }
     }
 
     // Function to highlight all enemies within range for attack
-    private void HighlightEnemies(List<Vector2Int> positions) {
-        foreach (Vector2Int pos in positions) {
+    private void HighlightEnemies(List<Vector2Int> positions)
+    {
+        foreach (Vector2Int pos in positions)
+        {
             HighlightTile(2, pos.x, pos.y);
         }
     }
 
     // Function to highlight commander
-    private void HighlightCommander(Commander commander) {
+    private void HighlightCommander(Commander commander)
+    {
         commanderModel = pieceLinks[(ChessPiece)commander].GetComponent<MeshRenderer>();
         commanderMaterial = commanderModel.material;
         commanderModel.material = commanderHighlightPrefab;
     }
 
     // Utility function to highlight any tile with the selected prefab at the x and y grid position
-    private void HighlightTile(int index, int x, int y) {
+    private void HighlightTile(int index, int x, int y)
+    {
         GameObject highlight = Instantiate(highlightPrefabs[index]);
         highlights.Add(highlight);
         highlight.transform.position = GetTileCenter(x, y) + Vector3.up * (index != 2 ? 0f : 0f);
     }
 
     // Utility function to destroy all highlight game objects
-    public void RemoveHighlights() {
-        foreach (GameObject highlight in highlights) {
+    public void RemoveHighlights()
+    {
+        foreach (GameObject highlight in highlights)
+        {
             Destroy(highlight);
         }
         highlights.Clear();
 
-        if (commanderMaterial != null && commanderModel != null) {
+        if (commanderMaterial != null && commanderModel != null)
+        {
             commanderModel.material = commanderMaterial;
             commanderModel = null;
             commanderMaterial = null;
@@ -315,7 +372,8 @@ public class ModelManager : MonoBehaviour
 
     #region UTILITY
     // Utility function to get the center position of a tile of the board game object
-    private Vector3 GetTileCenter(int x, int y) {
+    private Vector3 GetTileCenter(int x, int y)
+    {
         Vector3 origin = Vector3.zero;
         origin.x += (TILE_SIZE * x) + TILE_OFFSET;
         origin.z += (TILE_SIZE * y) + TILE_OFFSET;
@@ -323,20 +381,24 @@ public class ModelManager : MonoBehaviour
     }
 
     // Utility Debug function for testing raycasting and selection
-    private void DrawChessboard() {
+    private void DrawChessboard()
+    {
         Vector3 widthLine = Vector3.right * 8;
         Vector3 heightLine = Vector3.forward * 8;
 
-        for (int i = 0; i < 9; i++) {
+        for (int i = 0; i < 9; i++)
+        {
             Vector3 start = Vector3.forward * i;
             Debug.DrawLine(start, start + widthLine);
-            for (int j = 0; j < 9; j++) {
+            for (int j = 0; j < 9; j++)
+            {
                 start = Vector3.right * j;
                 Debug.DrawLine(start, start + heightLine);
             }
         }
 
-        if (selection.x >= 0 && selection.y >= 0) {
+        if (selection.x >= 0 && selection.y >= 0)
+        {
             Debug.DrawLine(
                 Vector3.forward * selection.y + Vector3.right * selection.x,
                 Vector3.forward * (selection.y + 1) + Vector3.right * (selection.x + 1));
@@ -349,7 +411,7 @@ public class ModelManager : MonoBehaviour
 
     #region HEURISTICS
     //take new positions and move piece and values to new tile; clear to null old tile.
-    public void BoardTileLocationUpdate(Vector2Int oldPosition, Vector2Int newPosition, bool isWhiteP, string pieceTypeC) 
+    public void BoardTileLocationUpdate(Vector2Int oldPosition, Vector2Int newPosition, bool isWhiteP, string pieceTypeC)
     {
         chessBoardGridCo[Convert.ToString(Convert.ToChar(oldPosition.x + 65) + Convert.ToString(oldPosition.y + 1))].isOccupied = false;
         chessBoardGridCo[Convert.ToString(Convert.ToChar(oldPosition.x + 65) + Convert.ToString(oldPosition.y + 1))].isWhite = false;
@@ -400,12 +462,12 @@ public class ModelManager : MonoBehaviour
         return posValue;
     }
 
-    public void BoardWideHeuristicCall() 
+    public void BoardWideHeuristicCall()
     {
         heuristics.BoardWideHeuristic(ref chessBoardGridCo);
     }
 
-    public void BoardWideHeuristicTileCall(int x, int y) 
+    public void BoardWideHeuristicTileCall(int x, int y)
     {
         Vector2Int holderV2I = new Vector2Int();
         holderV2I.x = 0;
@@ -421,67 +483,62 @@ public class ModelManager : MonoBehaviour
         chessBoardGridCo[Convert.ToString(Convert.ToChar(x) + Convert.ToString(y))].whiteHeuristic = holderV2I.x;
         chessBoardGridCo[Convert.ToString(Convert.ToChar(x) + Convert.ToString(y))].blackHeuristic = holderV2I.y;
     }
-    #endregion
-}
 
-public void  MakeBoardWideHuerCopy()
-{
-    BoardTile board = new BoardTile();
-    char letterBoard = '0';
-    string showB = "";
-
-    //setting up tiles and adding to dictionary default values
-    for (int j = 1; j < 9; j++)
+    private void MakeBoardWideHuerCopy()
     {
-        for (int i = 65; i < 73; i++)
+        BoardTile board = new BoardTile();
+        char letterBoard = '0';
+        string showB = "";
+
+        //setting up tiles and adding to dictionary default values
+        for (int j = 1; j < 9; j++)
         {
-            letterBoard = '0';
-            showB = "";
+            for (int i = 65; i < 73; i++)
+            {
+                letterBoard = '0';
+                showB = "";
 
-            letterBoard = Convert.ToChar(i);
-            showB = letterBoard.ToString() + j.ToString(); //place letter before number
-            board = new BoardTile();
+                letterBoard = Convert.ToChar(i);
+                showB = letterBoard.ToString() + j.ToString(); //place letter before number
+                
+                chessBoardCopyKing[showB].isOccupied = chessBoardGridCo[showB].isOccupied;
+                chessBoardCopyKing[showB].isWhite = chessBoardGridCo[showB].isWhite;
+                chessBoardCopyKing[showB].boardPosition.x = chessBoardGridCo[showB].boardPosition.x;
+                chessBoardCopyKing[showB].boardPosition.y = chessBoardGridCo[showB].boardPosition.y;
+                chessBoardCopyKing[showB].officalBoardPosition = chessBoardGridCo[showB].officalBoardPosition;
+                chessBoardCopyKing[showB].occupiedPieceType = chessBoardGridCo[showB].occupiedPieceType;
+                chessBoardCopyKing[showB].whiteHeuristic = chessBoardGridCo[showB].whiteHeuristic;
+                chessBoardCopyKing[showB].blackHeuristic = chessBoardGridCo[showB].blackHeuristic;
 
-            chessBoardCopyKing.Add(showB, board);
-            chessBoardCopyKing.isOccupied = chessBoardGridCo[showB].isOccupied;
-            chessBoardCopyKing.isWhite = chessBoardGridCo[showB].isWhite;
-            chessBoardCopyKing.boardPosition.x = chessBoardGridCo[showB].boardPosition.x;
-            chessBoardCopyKing.boardPosition.y = chessBoardGridCo[showB].boardPosition.y;
-            chessBoardCopyKing.officalBoardPosition = chessBoardGridCo[showB].officalBoardPosition;
-            chessBoardCopyKing.occupiedPieceType = chessBoardGridCo[showB].occupiedPieceType;
-            chessBoardCopyKing.whiteHeuristic = chessBoardGridCo[showB].whiteHeuristic;
-            chessBoardCopyKing.blackHeuristic = chessBoardGridCo[showB].blackHeuristic;
+                chessBoardCopyBishop1[showB].isOccupied = chessBoardGridCo[showB].isOccupied;
+                chessBoardCopyBishop1[showB].isWhite = chessBoardGridCo[showB].isWhite;
+                chessBoardCopyBishop1[showB].boardPosition.x = chessBoardGridCo[showB].boardPosition.x;
+                chessBoardCopyBishop1[showB].boardPosition.y = chessBoardGridCo[showB].boardPosition.y;
+                chessBoardCopyBishop1[showB].officalBoardPosition = chessBoardGridCo[showB].officalBoardPosition;
+                chessBoardCopyBishop1[showB].occupiedPieceType = chessBoardGridCo[showB].occupiedPieceType;
+                chessBoardCopyBishop1[showB].whiteHeuristic = chessBoardGridCo[showB].whiteHeuristic;
+                chessBoardCopyBishop1[showB].blackHeuristic = chessBoardGridCo[showB].blackHeuristic;
 
-            chessBoardCopyBishop1.Add(showB, board);
-            chessBoardCopyBishop1.isOccupied = chessBoardGridCo[showB].isOccupied;
-            chessBoardCopyBishop1.isWhite = chessBoardGridCo[showB].isWhite;
-            chessBoardCopyBishop1.boardPosition.x = chessBoardGridCo[showB].boardPosition.x;
-            chessBoardCopyBishop1.boardPosition.y = chessBoardGridCo[showB].boardPosition.y;
-            chessBoardCopyBishop1.officalBoardPosition = chessBoardGridCo[showB].officalBoardPosition;
-            chessBoardCopyBishop1.occupiedPieceType = chessBoardGridCo[showB].occupiedPieceType;
-            chessBoardCopyBishop1.whiteHeuristic = chessBoardGridCo[showB].whiteHeuristic;
-            chessBoardCopyBishop1.blackHeuristic = chessBoardGridCo[showB].blackHeuristic;
-
-            chessBoardCopyBishop2.Add(showB, board);
-            chessBoardCopyBishop2.isOccupied = chessBoardGridCo[showB].isOccupied;
-            chessBoardCopyBishop2.isWhite = chessBoardGridCo[showB].isWhite;
-            chessBoardCopyBishop2.boardPosition.x = chessBoardGridCo[showB].boardPosition.x;
-            chessBoardCopyBishop2.boardPosition.y = chessBoardGridCo[showB].boardPosition.y;
-            chessBoardCopyBishop2.officalBoardPosition = chessBoardGridCo[showB].officalBoardPosition;
-            chessBoardCopyBishop2.occupiedPieceType = chessBoardGridCo[showB].occupiedPieceType;
-            chessBoardCopyBishop2.whiteHeuristic = chessBoardGridCo[showB].whiteHeuristic;
-            chessBoardCopyBishop2.blackHeuristic = chessBoardGridCo[showB].blackHeuristic;
+                chessBoardCopyBishop2[showB].isOccupied = chessBoardGridCo[showB].isOccupied;
+                chessBoardCopyBishop2[showB].isWhite = chessBoardGridCo[showB].isWhite;
+                chessBoardCopyBishop2[showB].boardPosition.x = chessBoardGridCo[showB].boardPosition.x;
+                chessBoardCopyBishop2[showB].boardPosition.y = chessBoardGridCo[showB].boardPosition.y;
+                chessBoardCopyBishop2[showB].officalBoardPosition = chessBoardGridCo[showB].officalBoardPosition;
+                chessBoardCopyBishop2[showB].occupiedPieceType = chessBoardGridCo[showB].occupiedPieceType;
+                chessBoardCopyBishop2[showB].whiteHeuristic = chessBoardGridCo[showB].whiteHeuristic;
+                chessBoardCopyBishop2[showB].blackHeuristic = chessBoardGridCo[showB].blackHeuristic;
+            }
         }
     }
+    #endregion
 }
-
-public class BoardTile 
-{
-    public bool isOccupied;
-    public bool isWhite;
-    public Vector2Int boardPosition;//for vector 2 version of string position
-    public string officalBoardPosition;
-    public string occupiedPieceType;
-    public int whiteHeuristic;
-    public int blackHeuristic;
-}
+    public class BoardTile 
+    {
+        public bool isOccupied;
+        public bool isWhite;
+        public Vector2Int boardPosition;//for vector 2 version of string position
+        public string officalBoardPosition;
+        public string occupiedPieceType;
+        public int whiteHeuristic;
+        public int blackHeuristic;
+    }
